ce57d5d6f1629d619c06d46cbb929c4f299623ec
shakhnozatukhtaboeva <javanova1119@gmail.com>
2023-05-07 22:26:35 -0400

    Added Sales Page

--- src/HomePage2/ContactPage.java
+++
@@ -1,9 +0,0 @@
-package HomePage2;
-
-public class ContactPage {
-
-    //facebook
-    //twitter
-    //instagram
-    //emaol
-}
--- src/HomePage2/webapp2.java
+++
@@ -1,8 +0,0 @@
-package HomePage2;
-
-public class webapp2 {
-
-    //add more thing
-
-    // shahnoza master
-}
